d34fc5d66a3176e50ea0f110ba523a5420442397
prakhar308 <khandelwalprakhar30@gmail.com>
2020-06-20 10:52:08 +0530

    update cart to store nested items instead of array of items

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -8,9 +8,67 @@ import {
 } from '../constants/ActionTypes'
 
 const initialState = {
-   cart: [],
+   cart: {},
    loading: false,
    error: null
+}
+
+const fetchCart = (state, action) => {
+   const { cart } = action
+
+   // convert array of objects of cart items to
+   // nested objects where every cart item is stored by its id
+   // [{_id, qty, ...}, {_id, qty, ...}] => {_id: {qty, ...}, _id: {qty, ...}}
+   const transformedCart = cart.reduce((cartById, item) => {
+      let { _id, ...itemDetails } = item
+      cartById[_id] = itemDetails
+      return cartById
+   }, {})
+
+   return {
+      ...state,
+      cart: transformedCart
+   }
+}
+
+const addToCart = (state, action) => {
+   const { product } = action
+   const { _id, img, name, qty, price } = product;
+
+   // create new cart item
+   const cartItem = { img, name, qty, price } 
+
+   // insert the new cart item into the cart object
+   return {
+      ...state,
+      [_id]: cartItem
+   }
+}
+
+const updateCart = (state, action) => {
+   const { productId, qty } = action
+
+   return {
+      ...state,
+      cart: {
+         ...state.cart,
+         [productId]: {
+            ...state.cart[productId],
+            qty
+         }
+      } 
+   }
+}
+
+const removeFromCart = (state, action) => {
+   const { productId } = action
+   const { cart } = state
+   const { [productId]: value, ...remaining } = cart; 
+
+   return {
+      ...state,
+      cart: remaining
+   }
 }
 
 const cartReducer = (state = initialState, action) => {
@@ -21,36 +79,18 @@ const cartReducer = (state = initialState, action) => {
             loading: true
          }
       case FETCH_CART_SUCCESS:
-         return {
-            ...state,
-            cart: action.cart
-         }
+         return { ...fetchCart(state, action), loading: false }
       case ADD_TO_CART_SUCCESS:
-         return {
-            ...state,
-            cart: [...state.cart, action.product],
-            loading: false
-         }
+         return { ...addToCart(state, action), loading: false }
       case UPDATE_CART_SUCCESS:
-         return {
-            ...state,
-            cart: state.cart.map((product) => (
-               product._id === action.productId
-               ? {...product, qty: action.qty}
-               : product
-            ))
-         }
+         return { ...updateCart(state, action), loading: false }
       case REMOVE_CART_SUCCESS:
-         return {
-            ...state,
-            cart: state.cart.filter(product => (
-               product._id !== action.productId
-            ))
-         }
+         return {...removeFromCart(state, action), loading: false}
       case CART_OPERATION_FAIL:
          return {
             ...state,
-            error: action.error
+            error: action.error,
+            loading: false
          }
       default:
          return state;
