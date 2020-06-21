86047fd72b459ace8addf8ac6899408cad9d5243
prakhar308 <khandelwalprakhar30@gmail.com>
2020-06-21 09:24:23 +0530

    create separate component for displaying cart operation buttons

---
+++ src/components/CartOperationsButton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CartOperationsButton = ({
+   qty,
+   onAddToCart,
+   onUpdateCart,
+   onRemoveCart
+}) => {
+   
+   let update;
+   if(qty === 1)
+      update = () => onRemoveCart()
+   else
+      update = () => onUpdateCart(qty-1);
+
+   return (
+      qty === 0
+         ? <button
+               onClick={onAddToCart}>Add To Cart</button>
+         : <div>
+               <button onClick={update}>-</button>
+               <button>{qty}</button>
+               <button onClick={() => onUpdateCart(qty+1)}>+</button>
+           </div>
+   )
+}
+
+export default CartOperationsButton;
