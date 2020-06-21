1454e053edbe2403b363c862c68012ffa7c5989e
prakhar308 <khandelwalprakhar30@gmail.com>
2020-06-21 09:35:00 +0530

    add basic routing for HomePage and Cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 import HomePage from './HomePage';
+import Cart from '../containers/Cart'
 
 const App = (props) => {
    return (
-      <Switch>
-         <Route path="/" component={HomePage} />
-      </Switch>
+      <div>
+         <Link to="/" exact>Home</Link>
+         {' '}
+         <Link to="/cart">Cart</Link>
+         <Switch>
+            <Route path="/cart" component={Cart} />
+            <Route path="/" component={HomePage} />
+         </Switch>
+      </div>
    )
 }
 
