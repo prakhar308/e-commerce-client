dc22a56874911810d7cab5a0c2ec57076bdbce20
prakhar308 <khandelwalprakhar30@gmail.com>
2020-06-22 16:47:09 +0530

    update setAuthorizationHeader to delete the token from the header

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const setAuthorizationHeader = (token) => {
-   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+   if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+   } else {
+      delete axios.defaults.headers.common["Authorization"];
+   }
 }
