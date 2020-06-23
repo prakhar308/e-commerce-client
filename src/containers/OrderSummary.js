87fcc9ec7c22d28db319d7be621059f1ba925a07
prakhar308 <khandelwalprakhar30@gmail.com>
2020-06-23 20:28:18 +0530

    create OrderSummary component

---
+++ src/containers/OrderSummary.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const OrderSummary = (props) => {
+   const { cart, totalPrice } = props.cart;
+   return (
+      <div>
+         <h2>Order Summary</h2>
+         <p>You have {cart.length} items in your cart.</p>
+         {
+            cart.map((cartItem) => (
+               <div
+                  key={cartItem._id}>
+                  {cartItem.qty}{' '}x{' '}{cartItem.name}{' '}${cartItem.price}
+               </div>
+            ))
+         }
+         <p><strong>Total {totalPrice}</strong></p>
+      </div>
+   )
+}
+
+const mapStateToProps = state => ({
+   cart: state.cart
+})
+
+export default connect(mapStateToProps)(OrderSummary);
